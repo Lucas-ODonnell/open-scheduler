d9df893fd49e32cfa2ae599004c5e5cf53eae793
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2021-08-11 19:25:48 -0700

    added a create appointment modal

---
+++ app/javascript/src/components/Appointments/AppointmentModal.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const AppointmentModal = ({showModal, onClose, handleChange, newAppointment, handleSubmit}) => {
+	if (!showModal) return null;
+	return (
+		<div className="appointment-modal">
+			<div className="modal-content">
+				<div className="header">
+					<div>
+						<h2>Create an Appointment</h2>
+					</div>
+					<div className="close-button">
+						<button onClick={onClose}>&times;</button>
+					</div>
+				</div>
+				<form className="appointment-form" onSubmit={handleSubmit}>
+					<div className="field">
+						<label>Company Name: </label>
+						<input onChange={handleChange} type="text" name="company_name" value={newAppointment.company_name} placeholder="Company Name" />
+					</div>
+					<div className="field">
+						<label>Address:</label>
+						<input onChange={handleChange} type="text" name="street_address" value={newAppointment.street_address} placeholder="Main St" />
+					</div>
+					<div className="city-info">
+						<div className="field">
+							<label>City:</label>
+							<input onChange={handleChange} type="text" name="city" value={newAppointment.city} placeholder="NYC" />
+						</div>
+						<div className="field">
+							<label>State:</label>
+							<input onChange={handleChange} type="text" name="state" value={newAppointment.state} placeholder="NY" />
+						</div>
+						<div className="field">
+							<label>Zipcode:</label>
+							<input onChange={handleChange} type="number" name="zipcode" value={newAppointment.zipcode} placeholder="10001" />
+						</div>
+					</div>
+					<div className="country-row">
+						<div className="field">
+							<label>Country:</label>
+							<input onChange={handleChange} type="text" name="country" value={newAppointment.country} placeholder="USA" />
+						</div>
+						<div className="field">
+							<label>Contact:</label>
+							<input onChange={handleChange} type="text" name="company_contact" value={newAppointment.company_contact} placeholder="Davy Crockett" />
+						</div>
+					</div>
+					<div className="contact-information">
+						<div className="field">
+							<label>Phone:</label>
+							<input onChange={handleChange} type="text" name="phone" value={newAppointment.phone} placeholder="555-555-5555" />
+						</div>
+						<div className="field">
+							<label>Email:</label>
+							<input onChange={handleChange} type="email" name="email" value={newAppointment.email} placeholder="contact@company.com" />
+						</div>
+						<div className="field">
+							<label>Meeting:</label>
+							<input onChange={handleChange} type="datetime-local" name="meeting_date" value={newAppointment.meeting_date} />
+						</div>
+					</div>
+					<div className="field">
+						<label>Notes:</label>
+						<textarea onChange={handleChange} type="text" name="notes" value={newAppointment.notes}/>
+					</div>
+					<button className='submit' type='submit'>Submit</button>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default AppointmentModal;
--- app/javascript/src/components/Appointments/Appointments.js
+++ app/javascript/src/components/Appointments/Appointments.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Appointment from './Appointment';
+import AppointmentModal from './AppointmentModal';
 import './appointments.css';
 
 const Appointments = () => {
 	const [appointments, setAppointments] = useState([]);
+	const [newAppointment, setNewAppointment] = useState({
+		company_name: "",
+		street_address: "",
+		city: "",
+		state: "",
+		zipcode: 0,
+		country: "",
+		company_contact: "",
+		phone: "",
+		email: "",
+		meeting_date: "",
+		notes: ""
+	})
+	const [showModal, setShowModal] = useState(false);
 
 	useEffect(() => {
 		axios.get('/api/v1/appointments.json')
@@ -14,20 +29,53 @@ const Appointments = () => {
 			.catch( response => console.log(response));
 	}, [Appointments.length])
 
+	const handleChange = (e) => {
+		e.preventDefault();
+		setNewAppointment({...newAppointment, [e.target.name]: e.target.value})
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const csrfToken = document.querySelector('[name=csrf-token]').content;
+		axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+		axios.post('/api/v1/appointments', newAppointment )
+			.then(response => {
+				setAppointments([...appointments, response.data.data])
+				setNewAppointment({
+					company_name: "",
+					street_address: "",
+					city: "",
+					state: "",
+					zipcode: 0,
+					country: "",
+					company_contact: "",
+					phone: "",
+					email: "",
+					meeting_date: "",
+					notes: ""
+				})
+				setShowModal(false);
+			})
+			.catch(response => console.log(response));
+	}
+
 	const appointmentList = appointments.map((appointment, index) => {
-		const { city, company_contact, company_name, country, email, meeting_date, notes, phone, slug, state, street_address, zipcode } = appointment.attributes;
+		const { city, company_contact, company_name, country, email, meeting_date, notes, phone,state, street_address, zipcode } = appointment.attributes;
 		return (
 			<Appointment
 				key={index}
-				{...{city, company_contact, company_name, country, email, meeting_date, notes, phone, slug, state, street_address, zipcode}}
+				{...{city, company_contact, company_name, country, email, meeting_date, notes, phone, state, street_address, zipcode}}
 				/>
 		)
-	})
-
+	});
 	return (
 		<section className="appointments">
 			<div className="appointments-header">
 				<h1>Appointments</h1>
+			</div>
+			<div className="new-appointment">
+				<button className="modal-button" onClick={() => setShowModal(true)}>New Appointment</button>
+				<AppointmentModal onClose={() => setShowModal(false)} {...{showModal, handleChange, newAppointment, handleSubmit}}/>
 			</div>
 			<div className="appointments-grid">
 				{appointmentList}
