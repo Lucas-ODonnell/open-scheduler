3421d87d59f3da052e4222c6a1d4c7f4fd3df5be
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2021-08-17 13:12:01 -0700

    changed some styling and finished the navbar for now

--- app/javascript/src/components/Nav.js
+++ app/javascript/src/components/Nav.js
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => (
 	<nav>
-		<div className="navbar">
+		<div className="navigation">
+			<div className="nav-header">
+				<h1>Open Scheduler</h1>
+			</div>
+			<div className="navbar">
 				<ul>
-						<li>
-							<Link to="/">Appointments</Link>
-						</li>
-						<li>
-							<Link to="/user">Username</Link>
-						</li>
+					<li>
+						<Link to="/">Appointments</Link>
+					</li>
+					<li>
+						<Link to="/user">Username</Link>
+					</li>
 				</ul>
 			</div>
+		</div>
 	</nav>
 )
 
